d36ff19776b19eeecc9a5a86f8ed1a5bce6328a1
Yiqiu Wang <yiqiuw@mit.edu>
2020-12-02 16:32:13 -0500

    mini disc

--- miniDisc/orthant/miniDisc.C
+++ miniDisc/orthant/miniDisc.C
@@ -51,7 +51,7 @@ bool ortScanSerial(point<dim> c, floatT rSqr, point<dim>* P, intT n, vector<poin
     }
   }
   if (confCount > 0)
-    cout << "conflicts = " << confCount << "/" << n << endl;
+    cout << "conflicts = " << confCount << "/" << n << " (" << 100*floatT(confCount)/n << " %)" << endl;
 
   bool hasOut = false;
   for(intT i=0; i<dd; ++i) {
@@ -59,6 +59,63 @@ bool ortScanSerial(point<dim> c, floatT rSqr, point<dim>* P, intT n, vector<poin
       hasOut = true;
       support.push_back(P[idx[i]]);
     }
+  }
+  return hasOut;
+}
+
+template<int dim>
+bool ortScan(point<dim> c, floatT rSqr, point<dim>* A, intT n, vector<point<dim>>& support, floatT* distGlobal) {
+  typedef point<dim> pointT;
+
+  if (n<2000) return ortScanSerial(c, rSqr, A, n, support, distGlobal);
+
+  intT dd = intT(pow(2.0, dim));
+  intT P = getWorkers()*8;//todo tune
+  intT blockSize = (n+P-1)/P;
+  intT idx[dd*P];
+  floatT dist[dd*P];
+  for (intT i=0; i<dd*P; ++i) idx[i] = -1;
+  for (intT i=0; i<P; ++i) {
+    for (intT j=0; j<dd; ++j) {
+      dist[i*dd+j] = distGlobal[j];}
+  }
+
+  par_for_1(intT p=0; p<P; ++p) {
+    intT s = p*blockSize;
+    intT e = min((intT)(p+1)*blockSize,n);
+    intT* locIdx = idx + p*dd;
+    floatT* locDist = dist + p*dd;
+    for (intT i=s; i<e; ++i) {
+      floatT dSqr = A[i].distSqr(c);
+      if (dSqr > rSqr*1.000001) {//numerical stability
+        intT o = c.quadrant(A[i]);
+        if (dSqr > locDist[o]*1.000001) {//numerical stability
+          locDist[o] = dSqr;
+          locIdx[o] = i;}
+      }
+    }
+  }
+
+  intT idxGlobal[dd];
+  for (intT o=0; o<dd; ++o) idxGlobal[o] = -1;
+
+  for(intT p=0; p<P; ++p) {
+    for(intT o=0; o<dd; ++o) {
+      intT* locIdx = idx + p*dd;
+      floatT* locDist = dist + p*dd;
+      if (locIdx[o] != -1) {
+        if(locDist[o] > distGlobal[o]) {
+          idxGlobal[o] = locIdx[o];
+          distGlobal[o] = locDist[o];}
+      }
+    }
+  }
+
+  bool hasOut = false;
+  for(intT o=0; o<dd; ++o) {
+    if (idxGlobal[o] != -1) {
+      hasOut = true;
+      support.push_back(A[idxGlobal[o]]);}
   }
   return hasOut;
 }
@@ -119,8 +176,13 @@ ball<dim> miniDiscSamplingOrtSerial(point<dim>* P, intT n) {
   floatT dist[dd];
   for(intT i=0; i<dd; ++i) dist[i] = -1;
 
+  floatT sampleTime = 0;
+  floatT remainTime = 0;
+  timing t; t.start();
+
   intT scanned = 0;
   intT step = n/100;
+  intT fuse = 1;
   while (scanned < n) {
     vector<pointT> support;
     for(intT i=0; i<B.size(); ++i) {
@@ -130,13 +192,15 @@ ball<dim> miniDiscSamplingOrtSerial(point<dim>* P, intT n) {
     scanned += step;
 
     if (!found) {
-      break;
+      fuse--;
+      if (!fuse) break;
     } else {
       auto supportNew = vector<pointT>();
       B = miniDiscPlainSerial(&support[0], support.size(), supportNew, ballT());
       for(intT i=0; i<dd; ++i) dist[i] = -1;
     }
   }
+  sampleTime = t.next();
 
   cout << "---" << endl;
 
@@ -148,6 +212,10 @@ ball<dim> miniDiscSamplingOrtSerial(point<dim>* P, intT n) {
     bool found = ortScanSerial<dim>(B.center(), B.radius()*B.radius(), P, n, support, dist);
 
     if (!found) {
+      remainTime = t.stop();
+      cout << "sample-time = " << 100*sampleTime/(sampleTime+remainTime) << " %" << endl;
+      cout << "remain-time = " << 100*remainTime/(sampleTime+remainTime) << " %" << endl;
+
       return B;
     } else {
       auto supportNew = vector<pointT>();
@@ -155,64 +223,85 @@ ball<dim> miniDiscSamplingOrtSerial(point<dim>* P, intT n) {
       for(intT i=0; i<dd; ++i) dist[i] = -1;
     }
   }
+  remainTime = t.stop();
+  cout << "sample-time = " << 100*sampleTime/(sampleTime+remainTime) << " %" << endl;
+  cout << "remain-time = " << 100*remainTime/(sampleTime+remainTime) << " %" << endl;
+
   return B;
 }
 
 template<int dim>
-bool ortScan(point<dim> c, floatT rSqr, point<dim>* A, intT n, vector<point<dim>>& support, floatT* distGlobal) {
+ball<dim> miniDiscSamplingOrt(point<dim>* P, intT n) {
+  typedef ball<dim> ballT;
   typedef point<dim> pointT;
+  if (n < 2000)  return miniDiscSamplingOrtSerial(P, n);
 
-  if (n<2000) return ortScanSerial(c, rSqr, A, n, support, distGlobal);
+  intT sample = dim*3;
+  ballT B;
+  if (sample > n) {
+    vector<pointT> support;
+    return miniDiscPlain(P, sample, support, B);
+  } else {
+    vector<pointT> support;
+    B = miniDiscPlain(P, sample, support, B);
+  }
 
   intT dd = intT(pow(2.0, dim));
-  intT P = getWorkers()*8;//todo tune
-  intT blockSize = (n+P-1)/P;
-  intT idx[dd*P];
-  floatT dist[dd*P];
-  for (intT i=0; i<dd*P; ++i) idx[i] = -1;
-  for (intT i=0; i<P; ++i) {
-    for (intT j=0; j<dd; ++j) {
-      dist[i*dd+j] = distGlobal[j];}
-  }
+  floatT dist[dd];
+  for(intT i=0; i<dd; ++i) dist[i] = -1;
 
-  par_for_1(intT p=0; p<P; ++p) {
-    intT s = p*blockSize;
-    intT e = min((intT)(p+1)*blockSize,n);
-    intT* locIdx = idx + p*dd;
-    floatT* locDist = dist + p*dd;
-    for (intT i=s; i<e; ++i) {
-      floatT dSqr = A[i].distSqr(c);
-      if (dSqr > rSqr*1.000001) {//numerical stability
-        intT o = c.quadrant(A[i]);
-        if (dSqr > locDist[o]*1.000001) {//numerical stability
-          locDist[o] = dSqr;
-          locIdx[o] = i;}
-      }
+  floatT sampleTime = 0;
+  floatT remainTime = 0;
+  timing t; t.start();
+
+  intT scanned = 0;
+  intT step = n/100;
+  intT fuse = 1;
+  while (scanned < n) {
+    vector<pointT> support;
+    for(intT i=0; i<B.size(); ++i) {
+      support.push_back(B.support()[i]);}
+
+    bool found = ortScan<dim>(B.center(), B.radius()*B.radius(), P+scanned, step, support, dist);
+    scanned += step;
+
+    if (!found) {
+      fuse--;
+      if (!fuse) break;
+    } else {
+      auto supportNew = vector<pointT>();
+      B = miniDiscPlain(&support[0], support.size(), supportNew, ballT());
+      for(intT i=0; i<dd; ++i) dist[i] = -1;
     }
   }
+  sampleTime = t.next();
 
-  intT idxGlobal[dd];
-  for (intT o=0; o<dd; ++o) idxGlobal[o] = -1;
+  cout << "---" << endl;
 
-  for(intT p=0; p<P; ++p) {
-    for(intT o=0; o<dd; ++o) {
-      intT* locIdx = idx + p*dd;
-      floatT* locDist = dist + p*dd;
-      if (locIdx[o] != -1) {
-        if(locDist[o] > distGlobal[o]) {
-          idxGlobal[o] = locIdx[o];
-          distGlobal[o] = locDist[o];}
-      }
+  while (1) {
+    vector<pointT> support;
+    for(intT i=0; i<B.size(); ++i) {
+      support.push_back(B.support()[i]);}
+
+    bool found = ortScan<dim>(B.center(), B.radius()*B.radius(), P, n, support, dist);
+
+    if (!found) {
+      remainTime = t.stop();
+      cout << "sample-time = " << 100*sampleTime/(sampleTime+remainTime) << " %" << endl;
+      cout << "remain-time = " << 100*remainTime/(sampleTime+remainTime) << " %" << endl;
+
+      return B;
+    } else {
+      auto supportNew = vector<pointT>();
+      B = miniDiscPlain(&support[0], support.size(), supportNew, ballT());
+      for(intT i=0; i<dd; ++i) dist[i] = -1;
     }
   }
+  remainTime = t.stop();
+  cout << "sample-time = " << 100*sampleTime/(sampleTime+remainTime) << " %" << endl;
+  cout << "remain-time = " << 100*remainTime/(sampleTime+remainTime) << " %" << endl;
 
-  bool hasOut = false;
-  for(intT o=0; o<dd; ++o) {
-    if (idxGlobal[o] != -1) {
-      hasOut = true;
-      support.push_back(A[idxGlobal[o]]);}
-  }
-  return hasOut;
+  return B;
 }
 
 template<int dim>
@@ -274,10 +363,12 @@ void miniDisc(point<dim>* P, intT n) {
   ballT D;
   cout << "method = orthant-scan" << endl;
   if (serial) {
-    D = miniDiscOrtSerial(P, n);
+    //D = miniDiscOrtSerial(P, n);
     //D = miniDiscSamplingOrtSerial(P, n);
-  } else
-    D = miniDiscOrt(P, n);
+  } else {
+    //D = miniDiscOrt(P, n);
+    D = miniDiscSamplingOrt(P, n);
+  }
 
   cout << "seb-time = " << t0.stop() << endl;
 
