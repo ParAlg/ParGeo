27ccdf9111b5bb661c622950274abe0aeefd9103
Yiqiu Wang <yiqiuw@mit.edu>
2021-03-04 23:36:32 -0500

    Refactor ser gift wrap

--- convexHull3D/giftWrapSerial/hull.C
+++ convexHull3D/giftWrapSerial/hull.C
@@ -27,7 +27,8 @@
 #include "parlay/primitives.h"
 #include "parlay/sequence.h"
 #include "pairHash.h"
-#include "common/geometry.h"
+#include "geometry/point.h"
+#include "geometry/algebra.h"
 #include <queue>
 #include "hull.h"
 #ifdef WRITE
@@ -45,44 +46,50 @@ using namespace std;
  o--o/
  a   c
  */
-floatT signedVolume(point<3> a, point<3> b, point<3> c, point<3> d) {
+template <class pt>
+typename pt::floatT signedVolume(pt a, pt b, pt c, pt d) {
   return (a-d).dot(crossProduct3d(b-a, c-a))/6;
 }
 
-intT pivotOnEdge(intT q0, intT q1, parlay::sequence<point<3>> const &P) {
-  intT p = 0;
-  for (intT i=1; i<P.size(); ++i) {
+template <class pt>
+size_t pivotOnEdge(size_t q0, size_t q1, parlay::slice<pt*, pt*> P) {
+  size_t p = 0;
+  for (size_t i=1; i<P.size(); ++i) {
     if (signedVolume(P[q0], P[q1], P[p], P[i]) > 1e-7) //numeric knob
       p = i;
   }
   return p;
 }
 
-intT pivotOnEdge0(point<3> q0, point<3> q1, parlay::sequence<point<3>> const &P) {
-  intT p = 0;
-  for (intT i=1; i<P.size(); ++i) {
+template <class pt>
+size_t pivotOnEdge0(pt q0, pt q1, parlay::slice<pt*, pt*> P) {
+  size_t p = 0;
+  for (size_t i=1; i<P.size(); ++i) {
     if (signedVolume(q0, q1, P[p], P[i]) > 1e-7) //numeric knob
       p = i;
   }
   return p;
 }
 
-parlay::sequence<facet3d> hull3d(parlay::sequence<point<3>> &P) {//todo change to hull 3d
+parlay::sequence<facet3d<pargeo::point<3>>> hull3d(parlay::sequence<pargeo::point<3>> &P) {
+  using pt = pargeo::point<3>;
+  using facet3d = facet3d<pt>;
+
   size_t n = P.size();
 
 #ifdef WRITE
   ofstream myfile;
   myfile.open("point.txt", std::ofstream::trunc);
-  for(auto p: P)
-    myfile << p << endl;
+  for(size_t p=0; p<P.size(); ++p)
+    myfile << P[p] << p << endl;
   myfile.close();
 #endif
 
-  auto xCmp = [&](point<3> i, point<3> j) {
-		     return i[0] < j[0];};
-  intT p1 = parlay::min_element(P, xCmp) - &P[0];
+  auto xCmp = [&](pt i, pt j) {
+		return i[0] < j[0];};
+  size_t p1 = parlay::min_element(P, xCmp) - &P[0];
 
-  point<3> q = P[p1];
+  pt q = P[p1];
   q[1] += 1;//todo special case of having q in P where q only differ from p in x
 
 #ifdef WRITE
@@ -92,17 +99,17 @@ parlay::sequence<facet3d> hull3d(parlay::sequence<point<3>> &P) {//todo change t
   auto H = parlay::sequence<facet3d>();
 
   // Find initial facet
-  intT p2 = pivotOnEdge0(P[p1], q, P);
-  intT p3 = pivotOnEdge(p1, p2, P);
-  H.emplace_back(p1, p2, p3, P);
+  size_t p2 = pivotOnEdge0(P[p1], q, make_slice(P));
+  size_t p3 = pivotOnEdge(p1, p2, make_slice(P));
+  H.emplace_back(p1, p2, p3, make_slice(P));
 
 #ifdef WRITE
-  myfile << P[p1] << endl;
-  myfile << P[p2] << endl;
-  myfile << P[p3] << endl;
+  myfile << P[p1] << p1 << endl;
+  myfile << P[p2] << p1 << endl;
+  myfile << P[p3] << p1 << endl;
 #endif
 
-  queue<pair<intT, intT>> Q;
+  queue<pair<size_t, size_t>> Q;
   Q.push(make_pair(p2, p1));
   Q.push(make_pair(p3, p2));
   Q.push(make_pair(p1, p3));
@@ -113,15 +120,15 @@ parlay::sequence<facet3d> hull3d(parlay::sequence<point<3>> &P) {//todo change t
   T.mark(p3, p1);
 
   while (Q.size() > 0) {
-    pair<intT, intT> e = Q.front();
+    pair<size_t, size_t> e = Q.front();
     Q.pop();
     if (T.processed(e.first, e.second))
       continue;
-    intT q = pivotOnEdge(e.first, e.second, P);
+    size_t q = pivotOnEdge(e.first, e.second, make_slice(P));
 #ifdef WRITE
-    myfile << P[q] << endl;
+    myfile << P[q] << q << endl;
 #endif
-    H.emplace_back(e.first, q, e.second, P);
+    H.emplace_back(e.first, q, e.second, make_slice(P));
     Q.push(make_pair(H.back().b, H.back().a));
     Q.push(make_pair(H.back().c, H.back().b));
     Q.push(make_pair(H.back().a, H.back().c));
@@ -137,4 +144,4 @@ parlay::sequence<facet3d> hull3d(parlay::sequence<point<3>> &P) {//todo change t
   return H;
 }
 
-parlay::sequence<facet3d> hull3d(parlay::sequence<point<3>> &);
+parlay::sequence<facet3d<pargeo::point<3>>> hull3d(parlay::sequence<pargeo::point<3>> &);
