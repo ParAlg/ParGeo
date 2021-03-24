b2ed847b9d09f00df2a38fb5126a8b03041c8749
Yiqiu Wang <yiqiuw@mit.edu>
2021-03-24 18:55:10 -0400

    Knn fixes

--- KNN/py_kdTree/pyknn.cpp
+++ KNN/py_kdTree/pyknn.cpp
@@ -1,3 +1,9 @@
+#define VERBOSE
+
+#ifdef VERBOSE
+#include "common/get_time.h"
+#endif
+
 #include "kdtKnn.h"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -18,23 +24,51 @@ py::array_t<size_t> py_kdtknn(py::array_t<double, py::array::c_style | py::array
 
   size_t n = array.size() / dim;
 
+#ifdef VERBOSE
+  timer t;
+#endif
+
   if (dim == 2) {
+#ifdef VERBOSE
+    t.start();
+#endif
     parlay::sequence<pargeo::point<2>> array_vec(n);
     std::memcpy(array_vec.data(), array.data(), array.size() * sizeof(double));
+#ifdef VERBOSE
+    std::cout << "::copy-in-time = " << t.get_next() << std::endl;
+#endif
     parlay::sequence<size_t> result_vec = kdtKnn::kdtKnn<2, pargeo::point<2>>(array_vec, k);
+#ifdef VERBOSE
+    std::cout << "::knn-time = " << t.get_next() << std::endl;
+#endif
     auto result = py::array_t<size_t>(k * n);
     auto result_buffer = result.request();
     size_t *result_ptr = (size_t *) result_buffer.ptr;
     std::memcpy(result_ptr,result_vec.data(),result_vec.size()*sizeof(size_t));
+#ifdef VERBOSE
+    std::cout << "::copy-out-time = " << t.stop() << std::endl;
+#endif
     return result;
   } else if (dim == 3) {
+#ifdef VERBOSE
+    t.start();
+#endif
     parlay::sequence<pargeo::point<3>> array_vec(n);
     std::memcpy(array_vec.data(), array.data(), array.size() * sizeof(double));
+#ifdef VERBOSE
+    std::cout << "::copy-in-time = " << t.get_next() << std::endl;
+#endif
     parlay::sequence<size_t> result_vec = kdtKnn::kdtKnn<3, pargeo::point<3>>(array_vec, k);
+#ifdef VERBOSE
+    std::cout << "::knn-time = " << t.get_next() << std::endl;
+#endif
     auto result = py::array_t<size_t>(k * n);
     auto result_buffer = result.request();
     size_t *result_ptr = (size_t *) result_buffer.ptr;
     std::memcpy(result_ptr,result_vec.data(),result_vec.size()*sizeof(size_t));
+#ifdef VERBOSE
+    std::cout << "::copy-out-time = " << t.stop() << std::endl;
+#endif
     return result;
   } else
     throw std::runtime_error("Right now the code only supports 2d or 3d points.");
