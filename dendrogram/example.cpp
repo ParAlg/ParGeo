9605316e4d64c134c419b4ce6d47dc9b620d8c27
shangdiy <shangdiy@Mit.edu>
2021-03-26 16:24:44 +0000

    working example

---
+++ dendrogram/example.cpp
@@ -0,0 +1,40 @@
+#include <pybind11/pybind11.h>
+
+
+int add(int i, int j) {
+    return i + j;
+}
+
+
+class Pet
+{
+    public:
+        Pet(const std::string &name, int hunger) : name(name), hunger(hunger) {}
+        ~Pet() {}
+
+        void go_for_a_walk() { hunger++; }
+        const std::string &get_name() const { return name; }
+        int get_hunger() const { return hunger; }
+
+    private:
+        std::string name;
+        int hunger;
+};
+
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(example, m) {
+    // optional module docstring
+    m.doc() = "pybind11 example plugin";
+
+    // define add function
+    m.def("add", &add, "A function which adds two numbers");
+
+    // bindings to Pet class
+    py::class_<Pet>(m, "Pet")
+        .def(py::init<const std::string &, int>())
+        .def("go_for_a_walk", &Pet::go_for_a_walk)
+        .def("get_hunger", &Pet::get_hunger)
+        .def("get_name", &Pet::get_name);
+}
