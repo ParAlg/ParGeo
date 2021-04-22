26c203c99cfb39b27423390cad41f01ed51f0193
Yiqiu Wang <yiqiu_wang@icloud.com>
2021-04-22 01:19:22 -0400

    Updated pybinding test

--- pybindings/test.py
+++ pybindings/test.py
@@ -4,28 +4,34 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 from matplotlib.collections import LineCollection
 
+from pypargeo import loadPoints
 from pypargeo import DelaunayGraph
+from pypargeo import WghDelaunayGraph
 from pypargeo import GabrielGraph
+from pypargeo import WghGabrielGraph
 from pypargeo import KnnGraph
+from pypargeo import WghKnnGraph
 from pypargeo import BetaSkeleton
+from pypargeo import WghBetaSkeleton
 
 start_time = None
 
-def test(points, name, gen, savePlot = False, k = -1):
+def test(points, name, graphGen, savePlot = False, k = -1):
   dim = 2
   print("\n" + name)
 
   start_time = datetime.now()
   if k < 0:
-    edges = gen(points)
+    edges = graphGen(points)
   else:
-    edges = gen(points, k)
+    edges = graphGen(points, k)
   end_time = datetime.now()
   print("graph-gen-time: {}".format(end_time - start_time))
-  print("#-edges =", len(edges)/2)
+  print("#-edges =", len(edges))
 
   if savePlot:
-    lc = LineCollection(points[edges])
+    intEdges = edges[:,:2].astype(np.int32)
+    lc = LineCollection(points[intEdges])
     fig = plt.figure()
     plt.gca().add_collection(lc)
     plt.xlim(points[:,0].min()-.1, points[:,0].max()+.1)
@@ -39,11 +45,15 @@ def test(points, name, gen, savePlot = False, k = -1):
 
 points = np.random.random((100, 2))
 test(points, "rand-delaunay-100", DelaunayGraph, True)
+test(points, "rand-wgh-delaunay-100", WghDelaunayGraph, True)
 test(points, "rand-gabriel-100", GabrielGraph, True)
+test(points, "rand-wgh-gabriel-100", WghGabrielGraph, True)
 test(points, "rand-knn1-100", KnnGraph, True, 1)
 test(points, "rand-knn2-100", KnnGraph, True, 2)
+test(points, "rand-wgh-knn2-100", WghKnnGraph, True, 2)
 test(points, "rand-knn10-100", KnnGraph, True, 10)
 test(points, "beta-skeleton0.5-100", BetaSkeleton, True, 0.5)
 test(points, "beta-skeleton1-100", BetaSkeleton, True, 1)
+test(points, "beta-wgh-skeleton1-100", WghBetaSkeleton, True, 1)
 test(points, "beta-skeleton2-100", BetaSkeleton, True, 2)
 test(points, "beta-skeleton5-100", BetaSkeleton, True, 5)
