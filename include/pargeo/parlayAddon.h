0af622660d836f8b1279f3fb228f247482292a2d
Yiqiu Wang <yiqiu_wang@icloud.com>
2021-06-13 23:00:09 -0400

    Fix

--- include/pargeo/parlayAddon.h
+++ include/pargeo/parlayAddon.h
@@ -56,8 +56,8 @@ namespace parlay {
     size_t n = In.size();
     size_t l = num_blocks(n, _block_size);
 
-    sequence<size_t>* Sums[k];
-    for (size_t i=0; i<k; ++i) Sums[i] = new sequence<size_t>(l);
+    sequence<size_t> Sums[k];
+    for (size_t i=0; i<k; ++i) Sums[i] = sequence<size_t>(l);
 
     sliced_for(n, _block_size,
 	       [&](size_t i, size_t s, size_t e) {
@@ -70,14 +70,14 @@ namespace parlay {
 		   }
 		 }
 		 for (size_t x=0; x<k; ++x)
-		   Sums[x]->at(i) = c[x];
+		   Sums[x].at(i) = c[x];
 	       },
 	       fl);
 
     auto chunks = sequence<mySeq>(k);
     size_t m[k];
     for (size_t x=0; x<k; ++x) {
-      m[x] = scan_inplace(Sums[x]->cut(0, Sums[x]->size()), addm<size_t>());
+      m[x] = scan_inplace(Sums[x].cut(0, Sums[x].size()), addm<size_t>());
       chunks[x] = mySeq(m[x]);
     }
 
@@ -89,68 +89,67 @@ namespace parlay {
 		 for (size_t j = s; j < e; j++) {
 		   for (size_t x = 0; x < k; x++) {
 		     if (Fl[j] == x) {
-		       chunks[x][Sums[x]->at(i) + c[x]++] = In.at(j);
+		       chunks[x][Sums[x].at(i) + c[x]++] = In.at(j);
 		       break;
 		     }
 		   }
 		 }
 	       },
 	       fl);
-    for (size_t x=0; x<k; x++) delete Sums[x];
 
     return chunks;
   }
 
-  template <typename mySeq, typename boolSeq>
-  sequence<mySeq*> split_k(size_t k, mySeq* In,
-			   boolSeq const &Fl,
-			   flags fl = no_flag) {
-    size_t n = In->size();
-    size_t l = num_blocks(n, _block_size);
+  // template <typename mySeq, typename boolSeq>
+  // sequence<mySeq*> split_k(size_t k, mySeq* In,
+  // 			   boolSeq const &Fl,
+  // 			   flags fl = no_flag) {
+  //   size_t n = In->size();
+  //   size_t l = num_blocks(n, _block_size);
 
-    sequence<size_t>* Sums[k];
-    for (size_t i=0; i<k; ++i) Sums[i] = new sequence<size_t>(l);
+  //   sequence<size_t>* Sums[k];
+  //   for (size_t i=0; i<k; ++i) Sums[i] = new sequence<size_t>(l);
 
-    sliced_for(n, _block_size,
-	       [&](size_t i, size_t s, size_t e) {
-		 size_t c[k];
-		 for (size_t x=0; x<k; ++x) c[x] = 0;
+  //   sliced_for(n, _block_size,
+  // 	       [&](size_t i, size_t s, size_t e) {
+  // 		 size_t c[k];
+  // 		 for (size_t x=0; x<k; ++x) c[x] = 0;
 
-		 for (size_t j = s; j < e; j++) {
-		   for (size_t x=0; x<k; ++x) {
-		     if (Fl[j] == x) c[x]++;
-		   }
-		 }
-		 for (size_t x=0; x<k; ++x)
-		   Sums[x]->at(i) = c[x];
-	       },
-	       fl);
+  // 		 for (size_t j = s; j < e; j++) {
+  // 		   for (size_t x=0; x<k; ++x) {
+  // 		     if (Fl[j] == x) c[x]++;
+  // 		   }
+  // 		 }
+  // 		 for (size_t x=0; x<k; ++x)
+  // 		   Sums[x]->at(i) = c[x];
+  // 	       },
+  // 	       fl);
 
-    auto chunks = sequence<mySeq*>(k);
-    size_t m[k];
-    for (size_t x=0; x<k; ++x) {
-      m[x] = scan_inplace(Sums[x]->cut(0, Sums[x]->size()), addm<size_t>());
-      chunks[x] = new mySeq(m[x]);
-    }
+  //   auto chunks = sequence<mySeq*>(k);
+  //   size_t m[k];
+  //   for (size_t x=0; x<k; ++x) {
+  //     m[x] = scan_inplace(Sums[x]->cut(0, Sums[x]->size()), addm<size_t>());
+  //     chunks[x] = new mySeq(m[x]);
+  //   }
 
-    sliced_for(n, _block_size,
-	       [&](size_t i, size_t s, size_t e) {
-		 size_t c[k];
-		 for (size_t x = 0; x < k; ++x) c[x] = 0;
+  //   sliced_for(n, _block_size,
+  // 	       [&](size_t i, size_t s, size_t e) {
+  // 		 size_t c[k];
+  // 		 for (size_t x = 0; x < k; ++x) c[x] = 0;
 
-		 for (size_t j = s; j < e; j++) {
-		   for (size_t x = 0; x < k; x++) {
-		     if (Fl[j] == x) {
-		       chunks[x]->at(Sums[x]->at(i) + c[x]++) = In->at(j);
-		       break;
-		     }
-		   }
-		 }
-	       },
-	       fl);
-    for (size_t x=0; x<k; x++) delete Sums[x];
+  // 		 for (size_t j = s; j < e; j++) {
+  // 		   for (size_t x = 0; x < k; x++) {
+  // 		     if (Fl[j] == x) {
+  // 		       chunks[x]->at(Sums[x]->at(i) + c[x]++) = In->at(j);
+  // 		       break;
+  // 		     }
+  // 		   }
+  // 		 }
+  // 	       },
+  // 	       fl);
+  //   for (size_t x=0; x<k; x++) delete Sums[x];
 
-    return chunks;
-  }
+  //   return chunks;
+  // }
 
 } // End namespace
