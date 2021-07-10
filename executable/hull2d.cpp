6aa9584cf5e104bedf652362793e41e905b81ee0
Yiqiu Wang <yiqiu_wang@icloud.com>
2021-07-10 11:37:16 -0400

    Add back hull2d quickhull (pbbsbench impl)

---
+++ executable/hull2d.cpp
@@ -0,0 +1,40 @@
+#include "convexHull2d/quickHull/hull.h"
+
+#include <iostream>
+#include <algorithm>
+#include "pargeo/parseCommandLine.h"
+#include "parlay/parallel.h"
+#include "pargeo/getTime.h"
+#include "pargeo/point.h"
+#include "pargeo/pointIO.h"
+
+using namespace pargeo;
+using namespace pargeo::pointIO;
+
+template <class pt>
+void timeHull(parlay::sequence<pt> &P, int rounds, char const *outFile) {
+  timer t; t.start();
+
+  auto H = pargeo::hull2d::quickHull::compute<pt>(make_slice(P));
+
+  std::cout << "hull-time = " << t.get_next() << "\n";
+  std::cout << "hull-size = " << H.size() << "\n";
+
+  t.stop();
+}
+
+int main(int argc, char* argv[]) {
+  commandLine P(argc,argv,"[-o <outFile>] [-r <rounds>] <inFile>");
+  char* iFile = P.getArgument(0);
+  char* oFile = P.getOptionValue("-o");
+  int rounds = P.getOptionIntValue("-r",0);
+
+  int dim = readHeader(iFile);
+  if (dim != 2) {
+    std::cout << "Error, convexHull2D only takes 2d inputs, abort.\n";
+    abort();
+  }
+
+  parlay::sequence<pargeo::fpoint<2>> Points = readPointsFromFile<pargeo::fpoint<2>>(iFile);
+  timeHull<pargeo::fpoint<2>>(Points, rounds, oFile);
+}
