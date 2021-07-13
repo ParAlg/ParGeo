80f4cd1c71c4854e09cb6591d3811ec5f4b3b64d
Yiqiu Wang <yiqiu_wang@icloud.com>
2021-07-13 14:22:29 -0400

    Add sampling based orthant scan method for smallest enclosing ball

---
+++ include/enclosingBall/sampling/seb.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "parlay/sequence.h"
+#include "pargeo/point.h"
+#include "enclosingBall/ball.h"
+
+namespace pargeo {
+  namespace seb {
+
+    namespace sampling {
+      template<int dim>
+      pargeo::seb::ball<dim> compute(parlay::slice<pargeo::point<dim>*, pargeo::point<dim>*>);
+    }
+
+  }
+}
