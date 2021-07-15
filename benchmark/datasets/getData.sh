42c007b5c3e6b61bd12026c40d044ebb7c063f81
Yiqiu Wang <yiqiu_wang@icloud.com>
2021-07-15 13:12:01 -0400

    Add data script

---
+++ benchmark/datasets/getData.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget "https://www.dropbox.com/s/v55cmysmaibip7i/bunny.csv?dl=1" -O bunny.csv
+wget "https://www.dropbox.com/s/uz7gjwow1xwnqxd/armadillo.csv?dl=1" -O armadillo.csv
+wget "https://www.dropbox.com/s/uz7gjwow1xwnqxd/armadillo.csv?dl=1" -O dragon.csv
+wget "https://www.dropbox.com/s/jkzdjfx73snvy06/buddha.csv?dl=1" -O buddha.csv
+wget "https://www.dropbox.com/s/qb7ceev36976f4z/asian-dragon.csv?dl=1" -O asian-dragon.csv
