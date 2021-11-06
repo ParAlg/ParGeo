e25334bb71da34dd0d2df8e6eb4397fc9d6f0dda
Yiqiu Wang <yiqiu_wang@icloud.com>
2021-11-06 11:41:30 -0400

    Update dynKdTree  - test bounding box

--- test/dynKdTree_test.cpp
+++ test/dynKdTree_test.cpp
@@ -1,9 +1,16 @@
 #include <memory>
 
 #include "dataset/uniform.h"
+#include "parlay/sequence.h"
 #include "pargeo/pointIO.h"
 #include "pargeo/dynKdTree.h"
 #include "gtest/gtest.h"
+
+
+template <int dim>
+parlay::sequence<pargeo::point<dim>> data() {
+  return pargeo::uniformInPolyPoints<dim, pargeo::point<dim>>(200, 0, 1.0);
+}
 
 
 template <int dim>
@@ -23,12 +30,8 @@ inline void testKdTree(parlay::sequence<pargeo::point<dim>>& P) {
   tree1->kNN(P[0], 10);
 }
 
-template <int dim>
-parlay::sequence<pargeo::point<dim>> data() {
-  return pargeo::uniformInPolyPoints<dim, pargeo::point<dim>>(200, 0, 1.0);
-}
 
-TEST(kdTree_structure, testSerial2d) {
+TEST(dynKdTree, treeStructure) {
   using namespace pargeo;
   using namespace pargeo::pointIO;
 
@@ -38,6 +41,47 @@ TEST(kdTree_structure, testSerial2d) {
 
   testKdTree<dim>(P);
 }
+
+
+TEST(dynKdTree, boundingBox) {
+  using namespace pargeo::dynKdTree;
+  static const int dim = 2;
+
+  double data[12] = {
+    0.1, 0.1,
+    0.3, 0.3,
+    0.0, 0.0,
+    0.4, 0.4,
+    0.31, 0.1,
+    0.4, 0.2
+  };
+
+  parlay::sequence<pargeo::point<dim>> b1;
+  b1.emplace_back(data);
+  b1.emplace_back(data + 2);
+
+  parlay::sequence<pargeo::point<dim>> b2;
+  b2.emplace_back(data + 4);
+  b2.emplace_back(data + 6);
+
+  parlay::sequence<pargeo::point<dim>> b3;
+  b3.emplace_back(data + 8);
+  b3.emplace_back(data + 10);
+
+  auto bb1 = boundingBox<dim>(b1);
+  auto bb2 = boundingBox<dim>(b2);
+  auto bb3 = boundingBox<dim>(b3);
+
+  ASSERT_FALSE(bb1.contains(b2[0]));
+  ASSERT_TRUE(bb2.contains(b1[0]));
+  ASSERT_TRUE(bb1.compare(bb2) == boundingBox<dim>::overlap);
+  ASSERT_TRUE(bb2.compare(bb1) == boundingBox<dim>::include);
+  ASSERT_TRUE(bb1.compare(bb3) == boundingBox<dim>::exclude);
+  ASSERT_TRUE(bb3.compare(bb1) == boundingBox<dim>::exclude);
+  ASSERT_TRUE(bb2.compare(bb3) == boundingBox<dim>::include);
+  ASSERT_TRUE(bb3.compare(bb2) == boundingBox<dim>::overlap);
+}
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
