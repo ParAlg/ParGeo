9b7d501aff1e0bbb611fa4890f6bcc064f7e0043
Yiqiu Wang <yiqiu_wang@icloud.com>
2021-11-11 11:38:45 -0500

    Update dynKdTree executable

--- executable/dynKdTree.cpp
+++ executable/dynKdTree.cpp
@@ -14,7 +14,8 @@ using namespace pargeo::pointIO;
 using namespace pargeo::IO;
 
 template<int dim>
-void timeKnn(parlay::sequence<pargeo::point<dim>> &P, size_t k, char const *outFile) {
+void timeKnn(parlay::sequence<pargeo::point<dim>> &P,
+	     size_t k, int batches, char const *outFile) {
   using namespace pargeo::dynKdTree;
 
   int n = P.size();
@@ -25,12 +26,21 @@ void timeKnn(parlay::sequence<pargeo::point<dim>> &P, size_t k, char const *outF
   pargeo::timer t;
   t.start();
 
+  int base = n / 2;
+
   unique_ptr<rootNode<dim, point<dim>>>
-    tree(new rootNode<dim, point<dim>>(P, 0, n / 2));
+    tree(new rootNode<dim, point<dim>>(P, 0, base));
 
   std::cout << "build-time = " << t.get_next() << "\n";
 
-  tree->insert(P, n / 2, n);
+  std::cout << "batches = " << batches << std::endl;
+  int batchSize = (n - base) / batches;
+  int inserted = 0;
+  std::cout << "batch-size = " << batchSize << std::endl;
+  for (int i = 0; i < batches; ++ i) {
+    tree->insert(P, base + inserted, base + inserted + batchSize);
+    inserted += batchSize;
+  }
 
   std::cout << "size-after-insert = " << tree->size() << "\n";
 
@@ -62,28 +72,29 @@ int main(int argc, char* argv[]) {
   commandLine P(argc,argv,"[-k <param>] [-o <outFile>] <inFile>");
   char* iFile = P.getArgument(0);
   size_t k = P.getOptionIntValue("-k",1);
+  size_t batches = P.getOptionIntValue("-b",1);
   char* oFile = P.getOptionValue("-o");
 
   int dim = readHeader(iFile);
 
   if (dim == 2) {
     parlay::sequence<pargeo::point<2>> Points = readPointsFromFile<pargeo::point<2>>(iFile);
-    timeKnn<2>(Points, k, oFile);
+    timeKnn<2>(Points, k, batches, oFile);
   } else if (dim == 3) {
     parlay::sequence<pargeo::point<3>> Points = readPointsFromFile<pargeo::point<3>>(iFile);
-    timeKnn<3>(Points, k, oFile);
+    timeKnn<3>(Points, k, batches, oFile);
   } else if (dim == 4) {
     parlay::sequence<pargeo::point<4>> Points = readPointsFromFile<pargeo::point<4>>(iFile);
-    timeKnn<4>(Points, k, oFile);
+    timeKnn<4>(Points, k, batches, oFile);
   } else if (dim == 5) {
     parlay::sequence<pargeo::point<5>> Points = readPointsFromFile<pargeo::point<5>>(iFile);
-    timeKnn<5>(Points, k, oFile);
+    timeKnn<5>(Points, k, batches, oFile);
   } else if (dim == 6) {
     parlay::sequence<pargeo::point<6>> Points = readPointsFromFile<pargeo::point<6>>(iFile);
-    timeKnn<6>(Points, k, oFile);
+    timeKnn<6>(Points, k, batches, oFile);
   } else if (dim == 7) {
     parlay::sequence<pargeo::point<7>> Points = readPointsFromFile<pargeo::point<7>>(iFile);
-    timeKnn<7>(Points, k, oFile);
+    timeKnn<7>(Points, k, batches, oFile);
   } else {
     cout << "dim = " << dim << endl;
     throw std::runtime_error("dimension not supported");
